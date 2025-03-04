ef2a0d6acd1ea388cfdf2c32f0941456563ef42f
Campos <100108519+camposeduardo@users.noreply.github.com>
2025-03-04 15:59:30 -0300

    chore: initial project structure

---
+++ backend/src/main/java/com/camposeduardo/backend/AlbumRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.camposeduardo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumRecommenderApplication.class, args);
+	}
+
+}
