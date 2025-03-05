9da04baa7c56ffcc66e812c0a0ef1c7d46523a42
Campos <100108519+camposeduardo@users.noreply.github.com>
2025-03-05 02:50:52 -0300

    fix(SpotifyController): add SpotifyController class

---
+++ backend/src/main/java/com/camposeduardo/backend/controller/SpotifyController.java
@@ -0,0 +1,40 @@
+package com.camposeduardo.backend.controller;
+
+import com.camposeduardo.backend.dto.AccessTokenDTO;
+import com.camposeduardo.backend.dto.SpotifyAlbumInformationDTO;
+import com.camposeduardo.backend.service.SpotifyLoginService;
+import com.camposeduardo.backend.service.SpotifyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/album-recommender/api/v1")
+public class SpotifyController {
+
+    @Autowired
+    private SpotifyLoginService loginService;
+
+    @Autowired
+    private SpotifyService spotifyService;
+
+    @GetMapping("/auth/spotify-url")
+    public ResponseEntity<Map<String, String>> getSpotifyAuthUrl()  {
+        return ResponseEntity.ok().body(loginService.createUserAuthURL());
+    }
+
+    @PostMapping("/token")
+    public ResponseEntity<AccessTokenDTO> getAccessToken(@RequestParam String code,
+                                                @RequestParam String verifier)  {
+        return ResponseEntity.ok().body(loginService.requestAccessToken(code, verifier));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<SpotifyAlbumInformationDTO>> search(@RequestHeader("authorization") String token,
+                                                                   @RequestParam String search)  {
+        return ResponseEntity.ok().body(spotifyService.search(search, token));
+    }
+}
