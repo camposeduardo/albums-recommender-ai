5fef0c1237def224a6b7c613a6d1de54077489d4
Campos <100108519+camposeduardo@users.noreply.github.com>
2025-03-08 02:06:00 -0300

    feat(AlbumRecommenderController): add saveAlbums function

--- backend/src/main/java/com/camposeduardo/backend/controller/AlbumRecommenderController.java
+++ backend/src/main/java/com/camposeduardo/backend/controller/AlbumRecommenderController.java
@@ -2,7 +2,9 @@ package com.camposeduardo.backend.controller;
 
 
 import com.camposeduardo.backend.dto.SpotifyAlbumInformationDTO;
+import com.camposeduardo.backend.dto.SpotifyAlbumsDTO;
 import com.camposeduardo.backend.service.OpenAIService;
+import com.camposeduardo.backend.service.SpotifyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AlbumRecommenderController {
 
+    private final SpotifyService spotifyService;
     private final OpenAIService openAiService;
 
     @GetMapping("/recommender")
@@ -24,5 +27,13 @@ public class AlbumRecommenderController {
             @RequestHeader("authorization") String spotifyToken
                                                                    ) {
         return ResponseEntity.ok().body(openAiService.generateAlbumRecommendations(album, artist, spotifyToken));
+    }
+
+    @PutMapping("/save/albums")
+    public ResponseEntity<String> saveAlbums(
+            @RequestBody SpotifyAlbumsDTO spotifyAlbums,
+            @RequestHeader("authorization") String spotifyToken
+    ) {
+        return ResponseEntity.ok().body(spotifyService.saveSpotifyAlbums(spotifyAlbums, spotifyToken));
     }
 }
