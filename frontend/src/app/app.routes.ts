d5eda75878242eedd5f078c0349c0d9b42c7a83f
Campos <100108519+camposeduardo@users.noreply.github.com>
2025-03-12 21:08:13 -0300

    feat(Frontend): configure routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AlbumResultPageComponent } from './album-result-page/album-result-page.component';
+import { RecommendationPageComponent } from './recommendation-page/recommendation-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'albums', component: AlbumResultPageComponent},
+  {path: 'recommendation', component: RecommendationPageComponent},
+];
--- frontend/src/app/main-page/main-page.component.ts
+++ frontend/src/app/main-page/main-page.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from "../search-bar/search-bar.component";
-import { RecommendationPageComponent } from '../recommendation-page/recommendation-page.component';
-import { AlbumResultListComponent } from "../album-result-page/album-result-list/album-result-list.component";
-import { AlbumResultPageComponent } from "../album-result-page/album-result-page.component";
 import { HelpSectionComponent } from '../help-section/help-section.component';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [SearchBarComponent, RecommendationPageComponent, AlbumResultPageComponent, HelpSectionComponent],
+  imports: [SearchBarComponent, HelpSectionComponent, RouterOutlet],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
